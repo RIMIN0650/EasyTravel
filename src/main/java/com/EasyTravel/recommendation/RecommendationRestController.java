c7dd9a44f9b5cf7080b64ebc656c1c3d0b14a704
KwonLaptop <chris03232000@gmail.com>
2025-01-22 14:55:53 +0900

    [권민석]  - 게시물 추천

--- src/main/java/com/EasyTravel/recommendation/RecommendationRestController.java
+++ src/main/java/com/EasyTravel/recommendation/RecommendationRestController.java
@@ -1,5 +1,44 @@
 package com.EasyTravel.recommendation;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.EasyTravel.recommendation.domain.Recommendation;
+import com.EasyTravel.recommendation.service.RecommendationService;
+
+import jakarta.servlet.http.HttpSession;
+
+@RestController
 public class RecommendationRestController {
+	
+	@Autowired
+	private RecommendationService recommendationService;
+	
+	@PostMapping("/post/recommend")
+	public Map<String, String> reccomend(@RequestParam("postId") int postId, HttpSession session){
+		int userId = (Integer)session.getAttribute("userId");
+		
+		Recommendation recommendation = recommendationService.addReccomend(postId, userId);
+		
+		Map<String, String> resultMap = new HashMap<>();
+		
+		if(recommendation != null) {
+			resultMap.put("result", "success");
+		} else {
+			resultMap.put("result", "fail");
+		}
+		
+		return resultMap;
+		
+	}
+	
+	
+	
+	
 	
 }
--- src/main/java/com/EasyTravel/recommendation/service/RecommendationService.java
+++ src/main/java/com/EasyTravel/recommendation/service/RecommendationService.java
@@ -1,5 +1,29 @@
 package com.EasyTravel.recommendation.service;
 
-public class RecommendationService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.EasyTravel.recommendation.domain.Recommendation;
+import com.EasyTravel.recommendation.repository.RecommendationRepository;
+
+@Service
+public class RecommendationService {
+	
+	@Autowired
+	private RecommendationRepository recommendationRepository;
+	
+	public Recommendation addReccomend(int postId, int userId) {
+		
+		Recommendation recommendation = Recommendation.builder()
+														.postId(postId)
+														.userId(userId)
+														.build();
+		
+		return recommendationRepository.save(recommendation);
+		
+	}
+	
+	
+	
+	
 }
