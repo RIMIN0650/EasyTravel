cf4a717e2d86c4ef82ea3899aae299377e19642b
KwonLaptop <chris03232000@gmail.com>
2025-03-27 14:13:02 +0900

    [권민석]  - 배포 위한 설정

--- src/main/java/com/EasyTravel/EasyTravelApplication.java
+++ src/main/java/com/EasyTravel/EasyTravelApplication.java
@@ -2,9 +2,10 @@ package com.EasyTravel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EasyTravelApplication {
+public class EasyTravelApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EasyTravelApplication.class, args);
